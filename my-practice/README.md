0cbb6992d2d6a51476711306cf53df0d3207c16e
Ayinde Daniel <danivetlyrics@gmail.com>
2022-07-23 08:50:42 -0700

    m

---
+++ my-practice/README.md
@@ -0,0 +1,7 @@
+Don't take me serious...
+
+i'm nit real... i'm a universe of free will
+
+feel free to play around with my environment.
+
+P.S Dont go close to my girl
---
+++ my-practice/reverse-recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the reverse of a string
+ *
+ * Return: Always 0
+ */
+
+void _put_recursion(char *s)
+int main (void)
+{
+	_put_recursion("Print this line in reverse");
+
+	return (0);
+}
+
+void _put_recursion(char *s)
+{
+	if (*s[] = "\0")
+	{
+
+		_putchar(*s);
+		_put_recursion( s++ );
+	}
+	else
+		_putchar("\n);
+}
